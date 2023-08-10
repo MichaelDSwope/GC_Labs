5de0df512535e30bab5af2454be6f22773d2d021
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2023-08-10 19:17:09 -0400

    first Commit Assessment2

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assessment_2
+{
+
+
+
+    class program
+
+    //method named CreateStringArray()... takes 3 strings as parameters..method should make new string[] ..next method should add
+    //each string into ist own spot in the string[] and finally return the string[].
+
+
+    static void CreateStringArray()
+    {
+        string[] strings = new string[] { "1", "2", "3" };
+
+
+        List<int> numList = new List<int>();
+        return numList.Add(1);
+        return numList.Add(2);
+        return numList.Add(3);
+
+
+
+        //create static method named SumArray(int[]). method should add all the values in array together and return the sum
+        static string SumArray(int[] )
+        {
+
+            int[] arr = new int[] { 1, 2, 3 };
+            int sum = arr.Sum();
+            Console.WriteLine(sum);
+        }
+        // create the static method named RemoveNum(int List, int num)..check list Contains number parameter passed into method. if so remove
+        //Value from the List passed in return void
+        //return (void);
+
+        List<string> AddToList = new List<string>();
+        AddToList.Add("grapes");
+        AddToList.Add("oranges");
+        AddToList.Add("cherries");
+        //create static method AddToList() takes in a string into parameter. method should add the strings grapes,oranges,and string
+        //parameter to a list of strings  example cherries passed on to parameters.. method will return list grapes,oranges,cherries 
+        static
+
+        //create static method TryMe(int, int,)takes in 2 numbers..method should devide first parameter by the second parameter and return 9
+        //if there is a Arithmatic exception if no error return correct result from opperation.
+        static int TryMe(int, int)
+        {
+
+        }
+    }
+
+
+}
