0228d8f76ddd230d9a63989e25ae06e5807d6850
MichaelDSwopeVox <digitalwhiterabbithole@gmail.com>
2023-08-10 20:32:39 -0400

    last commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Assessment2
 
 
 
-    class program
+
 
     //method named CreateStringArray()... takes 3 strings as parameters..method should make new string[] ..next method should add
     //each string into ist own spot in the string[] and finally return the string[].
@@ -23,62 +23,101 @@ namespace Assessment2
         return numList.Add(2);
         return numList.Add(3);
 
+    }
 
+    //create static method named SumArray(int[]). method should add all the values in array together and return the sum
+    static string SumArray(int[] )
+    {
 
-        //create static method named SumArray(int[]). method should add all the values in array together and return the sum
-        static string SumArray(int[] )
+        int[] arr = new int[] { 1, 2, 3 };
+        int sum = arr.Sum();
+        int sum = 0;
+
+        foreach (int value in array)
         {
-
-            int[] arr = new int[] { 1, 2, 3 };
-            int sum = arr.Sum();
-            Console.WriteLine(sum);
+            sum += value;
         }
-        // create the static method named RemoveNum(int List, int num)..check list Contains number parameter passed into method. if so remove
-        //In this example, the RemoveNum method takes a List<int> named list and an integer named num as parameters.
+
+        return sum;
+
+    }
+    // create the static method named RemoveNum(int List, int num)..check list Contains number parameter passed into method. if so remove
+    //In this example, the RemoveNum method takes a List<int> named list and an integer named num as parameters.
+
+    //Value from the List passed in return void
+    //return (void)
+
+    public static void RemoveNum(List<int> list, int num)
+    {
+        if (list.Contains(num))
+        {
+            list.Remove(num);
+            Console.WriteLine($"Removed {num} from the list.");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is not present in the list.");
+        }
+    }
+}
+
+
+    List<int> numbers = new List<int> { 1, 2, 3 };
+
+    int numToRemove = 3;
+    ListHelper.RemoveNum(numbers, numToRemove);
+
+    Console.WriteLine("Updated list:");
+    foreach (int num in numbers)
+    {
+        Console.WriteLine(num);
 
 
         static void RemoveNum(List<int> list, int num)
-        {
-            if (list.Contains(num))
-            {
-                list.Remove(num);
-            }
-            //by removing a specific number from the list and then printing the updated list.
-            Console.WriteLine(list);
-        }
+    {
+    if (list.Contains(num))
+    {
+        list.Remove(num);
     }
-    
-    
-    
-
-    //Value from the List passed in return void
-    //return (void);
-
-        List<string> AddToList = new List<string>();
-        AddToList.Add("grapes");
-        AddToList.Add("oranges");
-        AddToList.Add("cherries");
-        //create static method AddToList() takes in a string into parameter. method should add the strings grapes,oranges,and string
-        //parameter to a list of strings  example cherries passed on to parameters.. method will return list grapes,oranges,cherries 
+//by removing a specific number from the list and then printing the updated list.
+//create static method AddToList() takes in a string into parameter. method should add the strings grapes,oranges,and string
+//parameter to a list of strings  example cherries passed on to parameters.. method will return list grapes,oranges,cherries 
+List<string> AddToList = new List<string>();
+AddToList.Add("grapes");
+AddToList.Add("oranges");
+AddToList.Add("cherries");
+        
        
-        public static string AddToList() { return AddToList; }
-        //create static method TryMe(int, int,)takes in 2 numbers..method should devide first parameter by the second parameter and return 9
-        //if there is a Arithmatic exception if no error return correct result from opperation.
-        public static int TryMe(int numerator, int denominator)
-        {
-            try
-            {
-                int result = numerator / denominator;
-                return result;
-            }
-            catch (ArithmeticException)
-            {
-                return 9;
-            }
-        }
+public static string AddToList() { return AddToList; }
 
 
-    
 
+
+
+//create static method TryMe(int, int,)takes in 2 numbers..method should devide first parameter by the second parameter and return 9
+//if there is a Arithmatic exception if no error return correct result from opperation.
+public static int TryMe(int numerator, int denominator)
+{
+    try
+    {
+        int result = numerator / denominator;
+        return result;
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Divide by zero - error.");
+        return 9;
+    }
+    catch (ArithmeticException)
+    {
+        Console.WriteLine("Arithmetic exception occurred.");
+        return 9;
+    }
+    int num1 = 18;
+    int num2 = 2;
+
+    int result = MathOperations.TryMe(num1, num2);
+    Console.WriteLine("Result: " + result);
 
 }
+        
